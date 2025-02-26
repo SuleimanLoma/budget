482e5c3b9c57023d7e0b6448cf5cde07702b433e
Suleiman Loma <suleimanloma@gmail.com>
2025-02-26 18:46:53 +0600

    1 version

---
+++ BudgetApp1/App/BudgetApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp1App.swift
+//  BudgetApp1
+//
+//  Created by Suleiman Loma on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetApp1/UI/Main/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BudgetApp1
+//
+//  Created by Suleiman Loma on 26/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
