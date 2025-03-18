c213b7884c09e29ce54fe28037ce13e9507b3ace
Suleiman Loma <suleimanloma@gmail.com>
2025-03-18 13:08:03 +0600

    ver 4

---
+++ BudgetApp1/UI/Main/View/Components/MainView+CellView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView+CellView.swift
+//  BudgetApp1
+//
+//  Created by Suleiman Loma on 13/3/25.
+//
+
+import SwiftUI
+
+struct MainView_CellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView_CellView()
+}
